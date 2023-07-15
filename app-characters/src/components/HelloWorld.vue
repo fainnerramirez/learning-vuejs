64f99d313f51f59945b82458dd5ff6d37a4d2b2b
Fainner Ramirez <fainnerramirez@gmail.com>
2023-07-15 15:54:50 -0500

    feat(frontend): create function getCharacters API

--- app-characters/src/components/HelloWorld.vue
+++ app-characters/src/components/HelloWorld.vue
@@ -1,40 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
+const characters = ref([])
 
-const count = ref(0)
+const getCharacters = async () =>  {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const { results } = await response.json();
+  characters.value = results;
+}
+
+getCharacters();
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container">
+      <div v-for="data in characters" :key="data.id" class="square">
+          <img :src="data.image"/>
+          <h4>{{ data.name }} - {{ data.species }}</h4>
+      </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
+ 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .square {
+    border: 5px solid #000000;
+
+  }
 </style>
