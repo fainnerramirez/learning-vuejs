323d47a46171f9b79fbfb210415c9d14bccc8353
Fainner Ramirez <fainnerramirez@gmail.com>
2023-07-16 18:33:18 -0500

    feat(frontend): create components

--- app-characters/src/components/CardPersonaje.vue
+++ app-characters/src/components/CardPersonaje.vue
@@ -1,24 +1,19 @@
-
 <script setup>
-import { ref } from 'vue'
-
-const characters = ref([])
-
-const getCharacters = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
-  const { results } = await response.json();
-  characters.value = results;
-}
-
-getCharacters();
+    defineProps(
+        [
+            "name",
+            "image",
+            "species"
+        ]
+    )
 </script>
 
 <template>
-    <div v-for="character in characters" class="card"  style="width: 18rem; margin: 10px;">
-        <img :src="character.image" class="card-img-top" alt="character.name">
+    <div class="card" style="margin: 10px; width: 18rem;">
+        <img :src="image" class="card-img-top" :alt="name">
         <div class="card-body">
             <p class="card-text">
-                {{ character.name }} - {{ character.species }}
+                {{ name }} - {{ species }}
             </p>
         </div>
     </div>
--- app-characters/src/components/Personajes.vue
+++ app-characters/src/components/Personajes.vue
@@ -1,18 +1,34 @@
 <script setup>
+import { ref } from 'vue'
 import CardPersonaje from './CardPersonaje.vue';
+
+const characters = ref([])
+
+const getCharacters = async () => {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const { results } = await response.json();
+  characters.value = results;
+  console.log(characters.value);
+}
+
+getCharacters();
 </script>
 
 <template>
-  <div class="container">
-    <CardPersonaje />
+  <div v-for="character in characters" class="container">
+    <CardPersonaje 
+      :name="character.name" 
+      :image="character.image" 
+      :species="character.species"/>
   </div>
 </template>
- 
+
 <style scoped>
 .container {
+  background-color: brown;
+  width: 1200px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
 }
 </style>
